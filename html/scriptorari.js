85c5469cb940057616c5b4c037f2e71d1bcdcdab
alexiatkd <alessia.chiappa@gmail.com>
2015-06-07 10:05:48 +0200

    colori tab livelli

--- html/scriptorari.js
+++ html/scriptorari.js
@@ -17,16 +17,44 @@ function ready(){
             var courses=JSON.parse(response);
             var el="";
             for(var i=0;i<courses.length;i++){
-                console.log(courses[i].ora);   
+                console.log(courses[i].ora);
+				
+				if(courses[i].livello=="BASIC"){
 
-                el+=  "<tr>"+
+                el+=  "<tr class="success">"+
                         "<td class='tab-livelli-colonna' id=corso><a href='acqua_fitness.html'>"+courses[i].nomeCorso+"</a></td>"+
                         "<td>"+courses[i].ora+"</td>"+
                         "<td'>"+courses[i].giorno+"</td>"+
                         "<td>"+courses[i].livello+"</td>"+
                         "<td>"+courses[i].istruttore+"</td>"+
                         "<td>"+courses[i].sala+"</td>"+
-                      "</tr>"  
+                      "</tr>" 
+				}
+				else{
+					if(courses[i].livello=="MEDIUM"){
+
+					el+=  "<tr class="warning">"+
+							"<td class='tab-livelli-colonna' id=corso><a href='acqua_fitness.html'>"+courses[i].nomeCorso+"</a></td>"+
+							"<td>"+courses[i].ora+"</td>"+
+							"<td'>"+courses[i].giorno+"</td>"+
+							"<td>"+courses[i].livello+"</td>"+
+							"<td>"+courses[i].istruttore+"</td>"+
+							"<td>"+courses[i].sala+"</td>"+
+						  "</tr>" 
+					}
+					else{
+							 el+=  "<tr class="danger">"+
+							"<td class='tab-livelli-colonna' id=corso><a href='acqua_fitness.html'>"+courses[i].nomeCorso+"</a></td>"+
+							"<td>"+courses[i].ora+"</td>"+
+							"<td'>"+courses[i].giorno+"</td>"+
+							"<td>"+courses[i].livello+"</td>"+
+							"<td>"+courses[i].istruttore+"</td>"+
+							"<td>"+courses[i].sala+"</td>"+
+						  "</tr>" 
+						
+					}
+					
+				}
 
 
             }
