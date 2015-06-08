e256f5fec436c1e28008214f4a80cef6230b357b
rocati <ro.cati91@gmail.com>
2015-06-08 18:34:58 +0200

    script orari

--- html/scriptorari.js
+++ html/scriptorari.js
@@ -9,7 +9,7 @@ function ready(){
         method: "POST",
         //dataType: "json", //type of data
         crossDomain: true, //localhost purposes
-        url: "selectorari.php", //Relative or absolute path to file.php file
+        url: "http://biggymchiappacatizzone.altervista.org/html/selectorari.php", //Relative or absolute path to file.php file
         data: {course:id},
         success: function(response) {
             console.log("pippo");
