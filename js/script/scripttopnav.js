29b2b784e65ee53b6debcffb41538ad8309d1517
alexiatkd <alessia.chiappa@gmail.com>
2015-06-13 00:07:27 +0200

    FORM DINAMICO

--- js/script/scripttopnav.js
+++ js/script/scripttopnav.js
@@ -14,17 +14,20 @@ function ready(){
             var el="";
             var el1="";
             var el2="";
+            var el3="";
             for(var i=0;i<corsi.length;i++){
                 var idCorso=corsi[i].idCorso;
                 var url="http://biggymchiappacatizzone.altervista.org/html/corso.html?id="+idCorso;
                 el+="<a href='"+url+"'>"+corsi[i].nomeCorso+"</a>";
+                el3+="<option>"+corsi[i].nomeCorso+"</option>";
+                
             }
 			el1+="<a href='../html/tutti_corsi.html'>Tutti i Corsi (A-Z)</a>";
             el2+="<a href='../html/tutti_corsi_livello.html'>Tutti i Corsi (Livelli)</a>";
             $("#tutticorsi").html(el1);
             $("#tuttilivello").html(el2);
             $("#eleconcorsi").html(el);
-            
+            $("#elenco").html(el3);
             
         },
         error: function(request,error) 
