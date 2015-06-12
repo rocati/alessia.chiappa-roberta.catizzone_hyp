25484a88283c59d7655388f8694354f6f2dd5252
alexiatkd <alessia.chiappa@gmail.com>
2015-06-12 23:26:33 +0200

    istruttori corsi

---
+++ js/script/scriptraccordocorsoistr.js
@@ -0,0 +1,34 @@
+//script che preleva preleva il JSON degli istruttori
+$(document).ready(ready);
+
+function ready(){
+    var id=getURLParameter("id");
+    $.ajax({
+        method: "POST",
+        //dataType: "json", //type of data
+        crossDomain: true, //localhost purposes
+        url: "../php/selectcorsi.php", //Relative or absolute path to file.php file
+        data: {"funzione": "istruttori","id":id},
+        success: function(response) {
+            var corsi=JSON.parse(response);
+            var el="";
+                        
+            for(var i=0;i<corsi.length;i++){
+              var url="http://biggymchiappacatizzone.altervista.org/html/istruttore.html?id="+corsi[i].idIstruttore;
+               el+="<a href='"+url+"' id='area-link'>"+corsi[i].istruttore+"</a><br><br>";
+        	}
+           
+           $(".campoistruttori").html(el);
+           
+        },
+        error: function(request,error) 
+        {
+            console.log("Error");
+        }
+    });
+    
+}
+
+  function getURLParameter(name) {
+  return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null
+}
--- php/selectcorsi.php
+++ php/selectcorsi.php
@@ -34,4 +34,22 @@ if($tipoFunzione=='elenco')
      print json_encode($corsi);
 }
 
+/**********************************************************************************************************
+				ROBERTA 12062015 aggiunto per footer area link
+***********************************************************************************************************/
+if($tipoFunzione=='istruttori')
+{
+	$id=$_POST['id'];
+    $sql ="SELECT c.idCorso as idCorso, i.id as idIstruttore, c.nomeCorso as corso, i.nome as istruttore FROM corsi as c join raccordo_ci as racc on c.idCorso = racc.idCorso join istruttore as i on racc.idIstruttore = i.id WHERE c.idCorso=".$id;
+    $result =DB::getDB()->query($sql);
+    while($row = mysqli_fetch_array($result))
+    {
+        $corsi[] = array("idCorso" => $row['idCorso'],"idIstruttore" => $row['idIstruttore'],"corso" => $row['corso'],"istruttore" => $row['istruttore']);
+    }
+
+
+    // JSON ENCODE DEPENDING BY BUTTON
+     print json_encode($corsi);
+ }
+
 ?>
