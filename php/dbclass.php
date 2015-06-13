f2124844ccb8f193271705daed61678b6a9d97a4
rocati <ro.cati91@gmail.com>
2015-06-13 12:29:26 +0200

    Update dbclass.php

--- php/dbclass.php
+++ php/dbclass.php
@@ -22,7 +22,7 @@ private function __clone() { }
 public static function getDB(){
 	//se non esiste la crea
          if (self::$instance == null){
-              self::$instance = new self("biggymchiappacatizzone.altervista.org", "biggymchiappacatizzone","AlessiaRoberta", "my_biggymchiappacatizzone");
+              self::$instance = new self("localhost", "root","", "my_biggymchiappacatizzone");
          }
          return self::$instance ;
      }
